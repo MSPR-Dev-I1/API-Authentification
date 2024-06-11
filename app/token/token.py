6865c2bd197c157d11ba8e5ffcfe0ceb92f8e0ce
vonkuro <vonkuro@gmail.com>
2024-06-11 21:49:01 +0200

    Addition of a function to verify if a particular access is in the token

--- app/token/token.py
+++ app/token/token.py
@@ -11,3 +11,13 @@ def encode_jwt(access_key_list, ):
     token = jwt.encode(payload, jwt_key, algorithm='HS512')
 
     return token
+
+def verify_access(access_key, token):
+    payload = jwt.decode(token, 'test_secret_key', algorithms=['HS512'])
+    try:
+        access_key_list = payload["accesses"]
+    except KeyError:
+        return False
+    for key in access_key_list:
+        if access_key== key: return True
+    return False
