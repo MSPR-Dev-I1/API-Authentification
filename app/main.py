535dfa65085528758d22ea43d93fb8fe7381c171
DenisJongmanee <jongmanee.denis@gmail.com>
2024-07-02 12:08:33 +0200

    feat(message): Ajout lifespan start up sub deconnexion

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ import os
 from fastapi import FastAPI
 from app.routers import auth
 from app.database.premier_schema import setup_database
+from app.messages.subscription import lifespan
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 origins = ["*"]
